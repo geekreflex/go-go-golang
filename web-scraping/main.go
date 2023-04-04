a1b50ac98dc6abe88b96a9e8d1addc5b986695fb
jerry <jerrynwosu007@gmail.com>
2023-04-04 13:06:00 +0100

    initial commit: go antics

---
+++ web-scraping/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("quotes.toscrape.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Response Code", r.StatusCode)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("error", err.Error())
+	})
+
+	c.Visit("https://quotes.toscrape.com/random")
+}
